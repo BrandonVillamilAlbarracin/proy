5aada6798035d124cd55e0b78d599a14f25b1e54
Brandon villamil <brandon.villamil@usantoto.edu.co>
2023-11-26 17:55:41 -0500

    jurados

---
+++ src/jurados/repositorio/juradoRepository.ts
@@ -0,0 +1,5 @@
+export const SQL_JURADO_CRUD = {
+    REGISTRO_JURADO: "INSERT INTO estudiantes (documento, nombre, apellidos, id_curso) VALUES ($1, $2, $3, $4);",
+    MESAS_JURADOS:"SELECT jurados.nombre AS nombre_jurado, cursos.id_curso FROM jurados JOIN cursos ON jurados.id_curso = cursos.id_curso;",
+    ELIMINAR_JURADO: "DELETE FROM cursos cur where cur.id_curso = $1",
+  };
